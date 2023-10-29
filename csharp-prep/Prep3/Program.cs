e62772ad8e7f6c9cbb72c411e49527e84a340a39
Dallin <122123363+DallinRWilliams@users.noreply.github.com>
2023-10-28 22:14:38 -0600

    Finished Prep3 assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        
+        int guess;
+        int guesses = 0;
+        guess = 0; 
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guessText = Console.ReadLine();
+            guess = int.Parse(guessText);
+            guesses += 1;
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"It took you {guesses} guesses.");
+            }
+        }
+
     }
 }
