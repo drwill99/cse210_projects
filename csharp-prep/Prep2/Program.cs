6e941cb25bcc69b57fc2ea35edf0d0540b3250c2
Dallin <122123363+DallinRWilliams@users.noreply.github.com>
2023-10-28 22:00:04 -0600

    Finished Prep2 assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.WriteLine("Enter a numeric grade (between 0 and 100):");
+        if (float.TryParse(Console.ReadLine(), out float studentGrade))
+        {
+            string letterGrade;
+
+            if (studentGrade >= 90)
+                letterGrade = "A";
+            else if (studentGrade >= 80)
+                letterGrade = "B";
+            else if (studentGrade >= 70)
+                letterGrade = "C";
+            else if (studentGrade >= 60)
+                letterGrade = "D";
+            else
+                letterGrade = "F";
+
+            Console.WriteLine($"Letter grade for {studentGrade:F2} is {letterGrade}");
+        }
     }
 }
