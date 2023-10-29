b07d49980267a4355e376f6a92d34a1c1a40eef6
Dallin <122123363+DallinRWilliams@users.noreply.github.com>
2023-10-28 22:33:16 -0600

    Finished Prep4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        do 
+        {
+        Console.WriteLine("Enter numbers to add to a list, when finished enter 0. ");
+        userNumber = int.Parse(Console.ReadLine());
+        
+        numbers.Add(userNumber);
+
+          
+        } while (userNumber != 0);
+
+        int total = 0;
+    
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The total of the list is: {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average of the list is: {average}");
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number you entered was: {largest}");
+
     }
+
 }
