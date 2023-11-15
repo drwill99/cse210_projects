f188ad87388d787662dce66f071581f041604362
Dallin <122123363+DallinRWilliams@users.noreply.github.com>
2023-11-15 14:19:52 -0700

    Fixed spacing issue in example scripture

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -2,9 +2,9 @@ using System;
 
 class Scripture
 {
-    private string referance = "Psalms 56:11";
+    private string referance = "Psalms 56:11 - ";
 
-    private List<string> scripture = new List<string> {"In", "God", "have", "I", "put", "my", "trust:", "I", "will", "not", "be", "afraid", "what", "man", "can", "do", "unto", "me."};
+    private List<string> scripture = new List<string> {"In ", "God ", "have ", "I ", "put ", "my ", "trust: ", "I ", "will ", "not ", "be ", "afraid ", "what ", "man ", "can ", "do ", "unto ", "me. "};
 
 
     public string getScripture()
