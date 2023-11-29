1ba517a37d06f3cb52252e5e709ca173feae9b93
Dallin <122123363+DallinRWilliams@users.noreply.github.com>
2023-11-29 10:00:42 -0700

    finished week 5 prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Creates a list to house all shapes
+        List<Shapes> shapes = new List<Shapes>();
+
+        // Gives parameters for all shapes here and adds them to list
+        Square ss = new Square("Orange", 7);
+        shapes.Add(ss);
+
+        Circle sc = new Circle("Purple", 3.2);
+        shapes.Add(sc);
+
+        Rectangle sr = new Rectangle("Yellow", 3, 4.5);
+        shapes.Add(sr);
+
+        // Uses a for-each loop to call each function and display each shape
+        foreach (Shapes s in shapes)
+        {
+            string color = s.getColor();
+
+            double area = s.getArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+
     }
+
+
 }
